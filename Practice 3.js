3c3cf710a68fbaedd9906c6d37a08ea5964ed47b
Yannick Van Oekelen <r0298651@student.thomasmore.be>
2016-11-26 19:01:34 +0100

    Practice 3: complete rework + meer afgeweken van de standaard oplossing van practice 2

--- Practice 3.js
+++ Practice 3.js
@@ -1,88 +1,78 @@
+
 /**
  * Created by yannickvanoekelen on 25/11/16.
  */
 //Shared with: Jeroen and Kristof//
 
 //Aanmaken van een array//
-var accounts = [ ];
+var accounts = [];
 
-//function createAccount aanmaken om een account aan te maken en te plaatsen in de array//
-function createAccount (account) {
-    accounts.push(account);
-    return account;
-}
-//function getAccount aanmaken om welbepaalde account op te vragen uit array//
-//function get account is gebasseerd op de methode van Luc met de forEach deze is aangepast met een while//
-function getAccount(username) {
-    var i = 0;
-    var matchedAccount;
-    while (i < accounts.length) {
-        if (accounts[i].name === username)
-            matchedAccount = accounts[i].name + " : " + accounts[i].balance + " AmountInEuro";
-        i++;
-    }
-    return matchedAccount;
-}
+//}
 
+//var bal = getBalance();
+//console.log(bal(accounts[0]));
+
+
+//function getBalance aanmaken om het huidige bedrag op een welbepaalde account te verkrijgen//
+function getBalance() {
+    return function (account) {
+        //console.log(account.balance);
+        return account.balance;
+
+    };
+}
+;
 //function deposit aanmaken om een bedrag te plaatsen op een welbepaald account//
 //Deze fuctie doet ook een validatie of men daadwerkelijk een bedrag geeft//
-function deposit (account, amount) {
-    if (typeof amount ==="number"){
-       account.balance += amount;
-        console.log("The account has been filled up");
+function deposit(account, amount) {
+    if (typeof amount === "number") {
+        account.balance += amount;
+        console.log("The account has been filled up with " + amount + " and has now " + account.balance + " Euro ");
 
-   }
-       else {
+    } else {
         console.log("the thing you just gave in was not a number");
     }
 }
 //function withdraw aanmaken om een bedrag af te halen van een welbepaald account//
 //deze functie doet ook een validatie of men daadwerkelijk een bedrag geeft//
-function withdraw (account, amount) {
-    if (typeof amount ==="number"){
+function withdraw(account, amount) {
+    if (typeof amount === "number") {
         account.balance -= amount;
-            console.log("the amount you gave in has been withdrawn from the account");
-    }
-        else {
-            console.log("the thing you just gave in was not a number");
+        console.log("Some money with the amount of " + amount + " has been withdrawn from the account it has now " + account.balance + " Euro left ");
+    } else {
+        console.log("the thing you just gave in was not a number");
     }
 }
-//function getBalance aanmaken om het huidige bedrag op een welbepaalde account te verkrijgen//
-function getBalance () {
-    return function (account) {
-        return account.balance;
-    };
+//function createAccount aanmaken om een account aan te maken en te plaatsen in de array//
+function createAccount(account) {
+    accounts.push(account);
 }
-var balance = getBalance();
-console.log(balance(accounts));
 
-var andrewsAccount = createAccount({
-    username: 'Andrew',
-    balance: 0
-});
+//function getAccount aanmaken om welbepaalde account op te vragen uit array//
+//function getAccount is gebasseerd op de methode van Luc met de forEach deze is aangepast met een while//
+function getAccount(username) {
+    var i = 0;
+    var matchedAccount;
+    while (i < accounts.length) {
+        if (accounts[i].name === username)
+            matchedAccount = accounts[i].name + " : " + accounts[i].balance + " Euro";
+        i++;
+    }
+    return matchedAccount;
+}
+;
 
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+var accountAndrew = {username: 'Andrew',
+    balance: 0};
+
+createAccount(accountAndrew);
 
 
-//functie deposit uitvoeren op andrewsAccount, we storten 100 op dit account//
-deposit(andrewsAccount,100);
-console.log(getBalance(andrewsAccount));
+getBalance(accounts);
 
-//functie withdraw uitvoeren op andrewsAccount, gaat er voor zorgen dat er 11 van dit account//
-withdraw(andrewsAccount, 5);
-console.log(getBalance(andrewsAccount));
 
-//we gaan hier de balance van andresAccount op het scherm plaatsen
-var existingAccount = getAccount('Andrew');
-console.log(getBalance(andrewsAccount));
+deposit(accounts[0], 2);
+withdraw(accounts[0], 55);
+console.log(getAccount("Andrew"));
 
-//we gaan hierbij een nieuw account aanmaken en in de array plaatsen
-var jensAccount = createAccount({
-    username: 'jen001',
-    balance: 12
-});
-console.log(accounts);
 
-//we gaan hier de balance van jensAccount op het scherm plaatsen
-var exitingJensAccount = getAccount('jen001');
-console.log(exitingJensAccount);
